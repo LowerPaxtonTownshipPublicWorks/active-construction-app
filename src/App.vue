bd8767139e9ff75dec654de59717af57719566d4
bbeattie <bbeatter@gmail.com>
2025-01-30 12:58:31 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import "@esri/calcite-components/dist/components/calcite-shell";
+import "@esri/calcite-components/dist/components/calcite-navigation";
+import "@esri/calcite-components/dist/components/calcite-navigation-logo";
+import "@arcgis/map-components/dist/components/arcgis-map";
+</script>
+
+<template>
+  <calcite-shell>
+    <calcite-navigation slot="header">
+      <calcite-navigation-logo slot="logo" heading="ArcGIS Vue Dependency Template" description="Ben Beattie" icon="map">
+
+      </calcite-navigation-logo>
+    </calcite-navigation>
+    <arcgis-map center="-76.3065, 40.0375" zoom="15" basemap="gray"></arcgis-map>
+  </calcite-shell>
+
+</template>
+
+<style>
+
+arcgis-map {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./assets/main.css"
+
+import { createApp } from "vue";
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+import App from "./App.vue";
+
+setAssetPath(location.href);
+
+createApp(App).mount("#app");
